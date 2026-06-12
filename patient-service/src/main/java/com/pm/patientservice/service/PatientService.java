cc1c09e2aa03db856387d9b007b8de55dcd8c7f9
hanuvadlamudi <hanuvadlamudivhr@gmail.com>
2026-06-12 08:23:41 +0530

    Initialize project and fix NullPointerException in PatientMapper

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,7 +1,11 @@
 package com.pm.patientservice.service;
 
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.mapper.PatientMapper;
+import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class PatientService {
@@ -12,7 +16,16 @@ public class PatientService {
         this.patientRepository = patientRepository;
     }
 
+    public List<PatientResponseDTO> getPatients(){
 
+        List<Patient> patients = patientRepository.findAll();
+
+        List<PatientResponseDTO> patientResponseDTOSList = patients.stream()
+                .map(patient -> PatientMapper.toDTO(patient))
+                .toList();
+
+        return patientResponseDTOSList;
+    }
 
 }
 
