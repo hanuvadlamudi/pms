8385e63767cea2a1ffc8bee12823261c755d3bc8
hanuvadlamudi <hanuvadlamudivhr@gmail.com>
2026-06-14 20:23:54 +0530

    add error handling

---
+++ patient-service/src/main/java/com/pm/patientservice/exception/EmailAlreadtExistsException.java
@@ -0,0 +1,8 @@
+package com.pm.patientservice.exception;
+
+public class EmailAlreadtExistsException extends RuntimeException {
+    public EmailAlreadtExistsException(String message) {
+
+        super(message);
+    }
+}
