f1a7c6b0e1d53bfec3ce3f219fc80472bed433fc
hanuvadlamudi <hanuvadlamudivhr@gmail.com>
2026-06-23 09:50:09 +0530

    connect PatientService with BillingServiceGrpcClient

---
+++ patient-service/src/main/java/com/pm/patientservice/grpc/BillingServiceGrpcClient.java
@@ -0,0 +1,37 @@
+package com.pm.patientservice.grpc;
+
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BillingServiceGrpcClient {
+
+    private final BillingServiceGrpc.BillingServiceBlockingStub blockingStub;
+
+    public BillingServiceGrpcClient(
+            @Value("${billing.service.address:localhost}") String serverAddress,
+            @Value("${billing.service.grpc.port;9001}") int serverPort
+    ){
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(serverAddress,serverPort)
+                .usePlaintext().build();
+
+        blockingStub = BillingServiceGrpc.newBlockingStub(channel);
+    }
+
+    public BillingResponse createBillingAccount(String patientId,String name,String email){
+
+        BillingRequest request = BillingRequest.newBuilder().setPatientId(patientId)
+                .setName(name).setEmail(email).build();
+
+        BillingResponse response = blockingStub.createBillingAccount(request);
+
+        return response;
+    }
+}
